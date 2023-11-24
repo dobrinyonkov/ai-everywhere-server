4b8a3fb1385729db07bc629fb0661ae3a339b14f
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-11-24 17:47:01 +0200

    migrate to koa and streams

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-import Koa from 'koa';
-import http from 'http';
-import fs from 'fs';
-const app = new Koa();
+import Koa from "koa";
+import serve from "koa-static";
+import http from "http";
+import { Readable } from "stream";
 
-console.log('Starting server...');
+import { Ollama } from "langchain/llms/ollama";
 
-app.use(async (ctx) => {
-    if (ctx.request.url === '/measurements.json') {
-        ctx.response.set('content-type', 'application/json');
-        // This is where the magic happens: set a stream as the response body
-        ctx.body = fs.createReadStream('./measurements.json');
-    }
+const ollama = new Ollama({
+  baseUrl: "http://ollama:11434",
+  model: "mistral",
 });
 
-http.createServer(app.callback()).listen(3030);
+class OllamaReadable extends Readable {
+  constructor(ollamaStream, options) {
+    super(options);
+    this.ollamaStream = ollamaStream;
+  }
+
+  async _read(size) {
+    const { value, done } = await this.ollamaStream.next();
+
+    if (done) {
+      this.push(null); // No more data
+    } else {
+      this.push(value);
+    }
+  }
+}
+
+const app = new Koa();
+
+const port = 3000;
+
+console.log("Starting server...");
+
+app.use(serve("./public"));
+
+app.use(async (ctx) => {
+  if (ctx.request.url.startsWith("/api/ask")) {
+    const question = ctx.query.question;
+
+    if (!question) {
+      ctx.status = 400;
+      ctx.body = { error: "Missing question" };
+      return;
+    }
+
+    const ollamaStream = await ollama.stream(
+      `Answer the following question: ${question}`
+    );
+
+    // Use the custom readable stream
+    const readableStream = new OllamaReadable(ollamaStream);
+
+    // Set the content type
+    ctx.response.set("content-type", "application/json");
+
+    // Set the custom readable stream as the response body
+    ctx.body = readableStream;
+  }
+});
+
+http.createServer(app.callback()).listen(port);
